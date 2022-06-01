cf7710beeba22c8e27b2b72a978d810b36bec4e9
tumuidle <7367204+tumuidle@user.noreply.gitee.com>
2022-06-02 06:59:32 +0800

    更新界面和协议，以及一个语音

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
 /**
- * By AFterNode H3xadecimal
- * Made with AutoJS APIv8
- * GNU GPLv3
+ * Made by AFterNode H3xadecimal
+ * Made with AutoJs Pro
+ * Voice from: 孙笑川
  */
 
+"ui";
 
+ui.layoutFile("./pages/main.xml")
+
+// 原生语音第一列
+VOX_SXC_SZO = "./res/VOX_SXC_SZO.mp3"
+ui.VOX_SXC_SZO.click(function(){
+    media.playMusic(VOX_SXC_SZO)
+})
+
+// 其它
+ui.STOP_ALL.click(function(){
+    media.stopMusic()
+})
